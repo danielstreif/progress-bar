87335eb275f196ad5a9329b69cc15295941b83d8
Daniel Streif <dan.streif@gmail.com>
2021-02-11 19:35:31 +0100

    add examples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import ProgressBar from "./ProgressBar";
 
 function App() {
-    return <ProgressBar percentage={42} />;
+    return (
+        <div style={{ maxWidth: "800px" }}>
+            <h1>ProgressBar component</h1>
+
+            <h2>Simple usage</h2>
+            <ProgressBar percentage={0} />
+            <ProgressBar percentage={1} />
+            <ProgressBar percentage={42} />
+            <ProgressBar percentage={99} />
+            <ProgressBar percentage={100} />
+
+            <h2>Value based usage</h2>
+            <ProgressBar value={20} minValue={0} maxValue={100} />
+
+            <h2>Error handling</h2>
+            <ProgressBar percentage={-4} />
+            <ProgressBar percentage={150} />
+            <ProgressBar maxValue={99} value={120} />
+            <ProgressBar minValue={10} value={-80} />
+        </div>
+    );
 }
 
 export default App;
